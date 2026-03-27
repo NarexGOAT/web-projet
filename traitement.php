fcb776d8978e6bcfbab746129614189b85cc0064
thêta54 <mohammednour.hokan@viacesi.fr>
2026-03-27 10:15:25 +0100

    création du fichier traitement.php

---
+++ traitement.php
@@ -0,0 +1,5 @@
+<?php
+// On demande à PHP d'afficher ce qu'il a reçu dans les "boîtes" (les names)
+echo "L'adresse email reçue est : " . $_POST['email'] . "<br>";
+echo "Le mot de passe reçu est : " . $_POST['mot_de_passe'];
+?>
