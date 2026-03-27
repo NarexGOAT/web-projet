02072f127e2926a295713f2dad3349699a9a8a15
RyanLSM <ryan.mourandee@viacesi.fr>
2026-03-27 14:42:42 +0100

    correction

--- database.php
+++ database.php
@@ -1,15 +1,24 @@
 <?php
+
 class Database {
+
     private $host = "localhost";
-    private $db_name = "stage"; // <--- METS LE NOM DE TA BDD ICI
+    private $db_name = "stage";
     private $username = "root";
-    private $password = ""; // Sur WSL, c'est souvent vide ou "root"
+    private $password = "";
 
     public function getConnection() {
         try {
-            $conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
+                $this->username,
+                $this->password
+            );
+
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             return $conn;
+
         } catch (PDOException $e) {
             echo "Erreur de connexion : " . $e->getMessage();
             return null;
